44613ff96f3866521eb4e9fbf18ae24dac0aeb48
The-HighO <olatunjiisaacjolaoluwa@gmail.com>
2023-05-12 09:33:33 +0100

    modeToggle

--- index.js
+++ index.js
@@ -5,9 +5,51 @@ setInterval(() => {
     let minutes = document.querySelector(".min")
     let seconds = document.querySelector(".sec")
 
+    let theHour = todaysDate.getHours()
+    let theMinute = todaysDate.getMinutes()
+    let theSeconds = todaysDate.getSeconds()
 
-    hours.innerHTML = todaysDate.getHours()
-    minutes.innerHTML = todaysDate.getMinutes()
-    seconds.innerHTML = todaysDate.getSeconds()
+    hours.innerHTML = theHour
+    minutes.innerHTML = theMinute
+    seconds.innerHTML = theSeconds
+    
+    // DOUBLE FIGURE CONDITION
+
+    if (theHour < 10) {
+        hours.innerHTML = "0" + theHour
+    }
+    if (theMinute < 10) {
+        minutes.innerHTML = "0" + theMinute
+    }
+    if (theSeconds < 10) {
+        seconds.innerHTML = "0" + theSeconds
+    }
+
+    // MODE SWITCH
+
 }, 10);
 
+let main = document.getElementById("mainContainer")
+let lightButton = document.querySelector(".toDark")
+let darktButton = document.querySelector(".toLight")
+let lightHourColor = document.querySelector(".colch")
+let lightMinColor = document.querySelector(".colch2")
+let lightSecColor = document.querySelector(".colch3")
+
+const modeToggle  = ()=>{
+    main.style.background = "#ffffff"
+    lightButton.style.display = "block"
+    darktButton.style.display = "none"
+    lightHourColor.style.color = "#090784"
+    lightMinColor.style.color = "#090784"
+    lightSecColor.style.color = "#090784"
+}
+
+const darkToggle = ()=>{
+    main.style.background = "#060606"
+    lightButton.style.display = "none"
+    darktButton.style.display = "block"
+    lightHourColor.style.color = "#060606"
+    lightMinColor.style.color = "#060606"
+    lightSecColor.style.color = "#060606"
+}
