c25d25fbef968d686033a7ba6406934222845aa5
The-HighO <olatunjiisaacjolaoluwa@gmail.com>
2023-05-20 09:46:52 +0100

    refactor

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+let hours = document.querySelector(".hour")
+let minutes = document.querySelector(".min")
+let seconds = document.querySelector(".sec")
 
 setInterval(() => {
     let todaysDate = new Date
-    let hours = document.querySelector(".hour")
-    let minutes = document.querySelector(".min")
-    let seconds = document.querySelector(".sec")
 
     let theHour = todaysDate.getHours()
     let theMinute = todaysDate.getMinutes()
